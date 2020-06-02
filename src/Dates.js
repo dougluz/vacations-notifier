ed4c1369b927eceb5de043366973c57ea1ebd0f4
dougluz <dougs.2210@gmail.com>
2020-06-02 17:39:51 -0300

    criado metodo para formatar as datas de dd/mm/yy para yyyy/mm/dd

--- src/Dates.js
+++ src/Dates.js
@@ -2,7 +2,8 @@
 export const formatDates = (vacations) => {
   vacations.map(item => {
     splitDates(item['Período Férias'])
-      .then(dates => console.log(dates))
+      .then(dates => stringToDate(dates))
+        .then(dates => console.log(dates))
   })
 }
 
@@ -11,6 +12,17 @@ const splitDates = (date) => {
     try {
       const dates = date.split(' ')
       resolve([dates[0], dates[2]])
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
+
+const stringToDate = (dates) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const vacations = dates.map(date => 20 + date.split('/').reverse().join('-'))
+      resolve(vacations)
     } catch (error) {
       reject(error)
     }
