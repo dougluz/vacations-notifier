92450be53f8632c27f4bd32b20a59217f6998e91
dougluz <dougs.2210@gmail.com>
2020-06-05 16:26:28 -0300

    agendado o envio de emails

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ import { readFile, copyFile } from './src/utils/File'
 import { formatDates } from './src/utils/Dates'
 import Vacations from './src/controllers/VacationsController'
 
-import connection from './src/config/Mongo'
+import dotenv from 'dotenv'
 
-function main () {
+dotenv.config()
 
+import { connection, closeConnection } from './src/config/Mongo'
+import { verifyVacations } from './src/utils/Vacations'
+import { sendMail } from './src/utils/Mail'
+
+function parse () {
   connection()
-
   readFile()
     .then(vacations => formatDates(vacations))
       .then(formated => formated.forEach(data => Vacations.create(data)))
@@ -16,11 +20,32 @@ function main () {
           .catch(error => console.log(error))
 }
 
-const parseData = new CronJob('0 0 23 * * *', () => {
-  main(),
+function mail () {
+  connection()
+  verifyVacations()
+    .then(vacations => {
+      if (vacations[0].length > 0 || vacations[1].length > 0) {
+        sendMail(vacations)
+      } else {
+        console.log('Não existem registros para serem enviados no momento')
+      }
+    }
+  )
+}
+  
+const parseJob = new CronJob('0 */3 * * * *', () => {
+  parse(),
   null,
   false,
   'America/Sao_Paulo'
 })
 
-parseData.start()
+const mailJob = new CronJob('0 */1 * * * *', () => {
+  mail(),
+  null,
+  false,
+  'America/Sao_Paulo'
+})
+
+parseJob.start()
+mailJob.start()
